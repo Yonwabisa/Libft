8b124c05f5a8a874ff932cbb51933285b7175380
Yonwabisa <yonwabisamphunga.yz@gmail.com>
2020-03-12 13:39:33 +0200

    this is not meant to be pushed

---
+++ libft.h
@@ -0,0 +1,13 @@
+#ifndef _LIBFT_H_
+#define _LIBFT_H_
+#include <unistd.h>
+#include <stdlib.h>
+
+char	*ft_strdup(const char *str);
+void	ft_strcpy(char *dest, char *src);
+int	ft_atoi(const char *str);
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+char	ft_toupper(int c);
+
+#endif
