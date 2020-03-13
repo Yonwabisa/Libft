86861577883b14035aa5ad8ed6b0ffbf30d22d22
Yonwabisa <yonwabisamphunga.yz@gmail.com>
2020-03-13 19:51:46 +0200

    added ft_strlen (which returns int insted of size_t) and ft_strcat's prototypes.

--- libft.h
+++ libft.h
@@ -2,13 +2,19 @@
 #define _LIBFT_H_
 #include <unistd.h>
 #include <stdlib.h>
-#include <stdio.h>
 
 char	*ft_strdup(const char *str);
 void	ft_strcpy(char *dest, char *src);
+void	ft_strncpy(char *dest, char *src, size_t n);
+int	ft_strcmp(const char *s1, const char *s2);
+char	*ft_strcat(char *dest, const char *src);
+int	ft_strlen(const char *str);
 int	ft_atoi(const char *str);
 int	ft_isalpha(int c);
 int	ft_isdigit(int c);
-char	ft_toupper(int c);
+int	ft_isprint(int c);
+
+int	ft_toupper(int c);
+int	ft_tolower(int c);
 
 #endif
