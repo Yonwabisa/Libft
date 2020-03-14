48b899832b56a6307afd0f7c1786a403cbb3ba39
Yonwabisa <yonwabisamphunga.yz@gmail.com>
2020-03-14 11:38:57 +0200

    removed the variable: int i

--- ft_strcat.c
+++ ft_strcat.c
@@ -2,16 +2,14 @@
 
 char	*ft_strcat(char *dest, const char *src)
 {
-	int i;
 	int j;
 	int k;
 	char *backup;
 
 	backup = 0;
 	k = 0;
-	i = ((ft_strlen(dest) + ft_strlen(src)) + 1);
 	j = (ft_strlen(dest) + 1);
-	backup = (char*)malloc(i + 1);
+	backup = (char*)malloc((ft_strlen(dest) + ft_strlen(src)) + 1);
 	ft_strcpy(backup, dest);
 	while (src[k])
 	{
